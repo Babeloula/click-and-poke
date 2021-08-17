fd801186f8480dd5799d7e20d5ba0a679e504234
Mathias Salanon <mat.salanon@gmail.com>
2021-08-17 15:12:43 +0200

    added the ability to book a pokestop

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :pokespots
+  resources :pokespots do
+    resources :bookings, only: [:create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
