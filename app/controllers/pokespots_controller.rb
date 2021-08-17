cf7b491257f59b42fc6c315d2ba026d9c3ed8b07
Walid Benzina <walid.benzina@yahoo.fr>
2021-08-17 11:41:37 +0200

    Change booking status to be an enum

--- app/controllers/pokespots_controller.rb
+++ app/controllers/pokespots_controller.rb
@@ -1,3 +1,19 @@
 class PokespotsController < ApplicationController
   authorize @restaurant
+
+  before_action :set_pokespot, only: [:edit, :update]
+
+  def edit
+  end
+
+  def update
+    @pokespot.update(pokespot_params)
+    redirect_to pokespots_path(@pokespot)
+  end
+
+  private
+
+  def set_pokespot
+    @pokespot = Pokespot.find(params[:id])
+  end
 end
